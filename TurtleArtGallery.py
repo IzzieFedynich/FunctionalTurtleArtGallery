2ddb78dc0c34e8993256f6499bf4871d77c777f2
IzzieFedynich <54276173+IzzieFedynich@users.noreply.github.com>
2019-09-10 08:46:10 -0500

    Update TurtleArtGallery.py

--- TurtleArtGallery.py
+++ TurtleArtGallery.py
@@ -1,11 +1,11 @@
 import turtle
 turtle.shape("turtle")
 turtle.speed(1000)
+
 def star(sidelength):
     for i in range(5):
         turtle.forward(sidelength)
         turtle.right(145)
-
 
 def MiniStars():
     for i in range(4):
@@ -14,10 +14,7 @@ def MiniStars():
         turtle.forward(75)
         turtle.pendown()
         star(50)
-MiniStars()
-turtle.penup()
-turtle.goto(345,345)
-turtle.pendown()
+
 def octogon(sidelength):
     for i in range(8):
         turtle.forward(sidelength)
@@ -28,28 +25,70 @@ def OctogonSpiral():
         octogon(i*2)
         turtle.right(5)
 
-OctogonSpiral()
-turtle.penup()
-turtle.goto(-300,-300)
-turtle.pendown()
-
 def square(sidelength):
     for i in range(4):
         turtle.forward(sidelength)
         turtle.right(90)
 
-def SquareSpiral():
+def SquareCircle():
     for i in range(77):
         square(1*20)
         turtle.right(5)
-SquareSpiral()
+
+def SquareSpiral():
+    for i in range(40):
+        square(i*5)
+        turtle.right(5)
+
 def triangle(sidelength):
     for i in range(3):
         turtle.forward(sidelength)
         turtle.right(130)
+
+def TriangleSpiral():
+    for i in range(25):
+        triangle(i*5)
+        turtle.right(5)
+
+turtle.bgcolor("light cyan")
+
+MiniStars()
+turtle.penup()
+turtle.goto(345,345)
+turtle.pendown()
+turtle.color("rosy brown")
+OctogonSpiral()
+turtle.penup()
+turtle.goto(-300,-300)
+turtle.pendown()
+turtle.color("sandy brown")
+SquareCircle()
 turtle.penup()
 turtle.goto(300,-300)
 turtle.pendown()
+turtle.color("gold")
+TriangleSpiral()
+turtle.color("orange")
+turtle.penup()
+turtle.goto(-320,300)
+turtle.pendown()
+TriangleSpiral()
+turtle.penup()
+turtle.goto(-400,0)
+turtle.color("light coral")
+turtle.pendown()
+SquareSpiral()
+turtle.penup()
+turtle.goto(400,0)
+turtle.pendown()
+turtle.color("light coral")
+SquareSpiral()
+turtle.penup()
+turtle.goto(0,100)
+turtle.pendown()
+turtle.color("Salmon")
+SquareCircle()
+
 
 
 turtle.exitonclick()
